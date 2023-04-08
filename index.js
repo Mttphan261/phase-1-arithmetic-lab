2338f6283762adc299aad167d0f79627a4876235
Matt Phan <mttphan261@gmail.com>
2023-04-08 15:40:32 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let multiply
+const num1 = 31;
+const num2 = 2;
+multiply = num1 * num2;
 
+let random
+random = Math.ceil(Math.random());
+
+let mod
+const num3 = 10;
+const num4 = 6;
+mod = num3 - num4;
+
+let max
+max = Math.max(1, 20);
 // Write your code here
